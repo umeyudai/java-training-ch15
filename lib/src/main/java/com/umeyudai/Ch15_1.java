a6fae14b30178dabe0434178674178a996c01bfd
umeyudai <ume.yudai@gmail.com>
2021-10-09 16:08:15 +0900

    inital commit

---
+++ lib/src/main/java/com/umeyudai/Ch15_1.java
@@ -0,0 +1,21 @@
+package com.umeyudai;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class Ch15_1 {
+    public static void main(String[] args) {
+		Path path = Paths.get("openjdk.txt");
+		try(BufferedReader in = Files.newBufferedReader(path)){
+			String line;
+			while((line=in.readLine())!=null) {
+				System.out.println(line);
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace(); 
+		}
+
+	}
+}
